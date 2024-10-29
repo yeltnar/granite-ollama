2f376cc9bba75fc5da03ba6401eb7ff7ea4dad45
Drew Brantley <drew@drew-mbp.local>
2024-10-29 10:56:47 -0500

    note on using GPU in start script

--- r.sh
+++ r.sh
@@ -1,4 +1,8 @@
 podman rm -f ollama;
 podman rm -f open-webui;
+
 # use the podman args to force the 'gpus' argument to be passed to the 'run' process 
-podman-compose --podman-run-args="--gpus=all" up;
+# podman-compose --podman-run-args="--gpus=all" up;
+
+# leave out to not fuss with GPUs 
+podman-compose up;
